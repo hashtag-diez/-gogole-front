8b23a0cbac8440d7ce305b5a6243969344e17957
kamaiel <raph.francoise@orange.fr>
2024-03-28 15:54:08 +0100

    book page ok

--- src/book.ts
+++ src/book.ts
@@ -1,7 +1,70 @@
+import { Book } from "./types";
+import { redirectToBookPage } from "./index";
+
 const urlParams = new URLSearchParams(window.location.search);
-const bookId = urlParams.get('bookId');
+const bookId = urlParams.get('bookId') || '';
+const ul = document.querySelector("ul")
 
+window.addEventListener("load", async () => {
+    //get from session storage
+    const res = await fetch("http://localhost:3333/search/?id="+bookId)
+    const book: Book = await res.json()
+    buildBook(book)
+    const suggestionsRes = await fetch("http://localhost:3333/suggestions/?id="+book.id)
+    const suggestionsJson = await suggestionsRes.json()
+    const suggestions: Book[] = suggestionsJson[0].similar
+    buildSuggestions(suggestions)
+}) 
 
-window.addEventListener("load", () => {
+const buildSuggestions = (suggestions: Book[]) => {
+    if (ul!=null ){
+      ul.innerHTML = ""
+      suggestions.forEach(suggestion => {
+          const li = document.createElement("li")
+          const image = document.createElement("img")
+          const legend  = document.createElement("legend")
+          const bookTitle = document.createElement("span")
+          const bookAuthor = document.createElement("span")
+          const authors = JSON.parse(suggestion.author)[0]
+          bookTitle.className = "titre"
+          bookTitle.textContent = suggestion.title
+          bookAuthor.className = "author"
+          bookAuthor.textContent = authors ? authors.name : "Auhtor undefined"
+          legend.appendChild(bookTitle)
+          legend.appendChild(bookAuthor)
+          image.src = suggestion.imagepath
+          image.addEventListener("click", () => redirectToBookPage(suggestion.id))
+          li.appendChild(image)
+          li.appendChild(legend)
+          ul.appendChild(li)
+      })
+    }
+}
 
-})
+const buildBook = (book: Book) => {
+    const authors = JSON.parse(book.author)[0]
+    const bookDiv = document.querySelector(".book")
+    const image = document.createElement("img")
+    const legend  = document.createElement("legend")
+    const bookTitle = document.createElement("span")
+    const bookAuthor = document.createElement("span")
+    const bookFileDiv = document.createElement("span")
+    const bookFile = document.createElement("a")
+    bookFileDiv.textContent = "Read the book : "
+    bookFileDiv.appendChild(bookFile)
+    bookFile.textContent = book.filepath
+    bookFile.href = book.filepath
+    bookTitle.className = "titre"
+    bookTitle.textContent = book.title
+    bookAuthor.className = "author"
+    bookAuthor.textContent = authors ? authors.name : "Auhtor undefined"
+    legend.appendChild(bookTitle)
+    legend.appendChild(bookAuthor)
+    legend.appendChild(bookFileDiv)
+    image.src = book.imagepath
+    if(bookDiv!=null){
+        bookDiv.appendChild(image)
+        bookDiv.appendChild(legend)
+    }
+    return bookDiv
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Book } from "./types.js"
 
+
+// Debounce function
 const debounce = (callback: (args: any) => void, delay: number = 1000) => {
     let timoutId: ReturnType<typeof setTimeout>;
     return function (this: any, event: Event){
@@ -22,22 +24,19 @@ input?.addEventListener('input', debounce(async (event: Event) => {
     
 }))
 
-const buildBook = (img: string, title: string, author: string, theme: string, bookId: number) => {
+// Build books
+export const buildBook = (img: string, title: string, author: string, theme: string, bookId: number) => {
     const book = document.createElement("li")
     const image = document.createElement("img")
     const legend  = document.createElement("legend")
     const bookTitle = document.createElement("span")
     const bookAuthor = document.createElement("span")
-    // const bookTheme = document.createElement("span")
     bookTitle.className = "titre"
     bookTitle.textContent = title
     bookAuthor.className = "author"
     bookAuthor.textContent = author
-    // bookTheme.className = "theme"
-    // bookTheme.textContent = theme
     legend.appendChild(bookAuthor)
     legend.appendChild(bookTitle)
-    // legend.appendChild(bookTheme)
     image.src = img
     image.addEventListener("click", () => redirectToBookPage(bookId))
     book.appendChild(image)
@@ -45,36 +44,43 @@ const buildBook = (img: string, title: string, author: string, theme: string, bo
     return book
 }
 
-const redirectToBookPage = (bookId: number) => {
-    const bookPageUrl = `./src/pages/book.html?bookId=${bookId}`;
-    window.location.href = bookPageUrl;
-}
-
 let page = 1
-let maxPages: number = -1
 const ul = document.querySelector("ul")
 const fetchBooks = async () => {
     const res = await fetch("http://localhost:3333/books/?page="+page)
     const books = await res.json()
-    if (maxPages == -1) maxPages = books[1]/9
-    if(ul!=null) {
-        books[0].forEach((book: Book) => {
+    if(ul!=null && books.length>0) {
+        books.forEach((book: Book) => {
             const authors = JSON.parse(book.author)[0]
-            ul.appendChild(buildBook(book.imagepath, book.title, authors ? authors.name : "Auhtor undefined" , "theme", book.id))      
+            const title = book.title.length > 30 ? book.title.slice(0, 30) + "..." : book.title
+            ul.appendChild(buildBook(book.imagepath, title, authors ? authors.name : "Auhtor undefined" , "theme", book.id))      
         });
     } 
 }
-
+const main = document.querySelector("main")
+const loading = document.querySelector("span[class='loading']")
 const fetchBooksSearch = async (query: string) => {
-    const res = await fetch("http://localhost:3333/search/?query="+query)
-    const books = await res.json()
-    if(ul!=null) {
-        ul.innerHTML = ""
-        books.forEach((book: Book) => {
-            const authors = JSON.parse(book.author)[0]
-            ul.appendChild(buildBook(book.imagepath, book.title, authors ? authors.name : "Auhtor undefined" , "theme", book.id))      
-        });
+    if (ul!=null) ul.innerHTML = ""
+    if (loading!=null){
+        loading.textContent = "Loading..."
+        main?.appendChild(loading)
+        const res = await fetch("http://localhost:3333/search/?query="+query)
+        const books = await res.json()
+        if(ul!=null && books.length>0) {
+            books.forEach((book: Book) => {
+                const authors = JSON.parse(book.author)[0]
+                const title = book.title.length > 30 ? book.title.slice(0, 30) + "..." : book.title
+                ul.appendChild(buildBook(book.imagepath, title, authors ? authors.name : "Auhtor undefined" , "theme", book.id))      
+            });
+        }
+        loading.innerHTML = ""
     }
+}
+
+// Redirect to book page
+export const redirectToBookPage = (bookId: number) => {
+    const bookPageUrl = `/src/pages/book.html?bookId=${bookId}`;
+    window.location.href = bookPageUrl;
 }
 
 window.addEventListener("load", fetchBooks)
@@ -88,4 +94,10 @@ window.addEventListener("scroll", () => {
         page++
         fetchBooks()
     }
+})
+
+// Onclick h2
+const h2 = document.querySelector("h2")
+h2?.addEventListener("click", () => {
+    window.location.href = "/";
 })
